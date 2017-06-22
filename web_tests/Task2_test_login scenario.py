aea311e0077e0fd71e7bc24077c44ccabb9dd6a9
Ogofman <olenagofman@gmail.com>
2017-06-22 07:06:13 +0300

    added correct name

--- web_tests/Task2_test_login scenario.py
+++ web_tests/Task2_test_login scenario.py
@@ -7,6 +7,6 @@ def test_open_chrome():
     driver.get("http://localhost/litecart/admin/login.php")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
-    driver.find_element_by_css_selector(".btn.btn-default").click()
+    driver.find_element_by_css_selector("button[name=login]").click()
     WebDriverWait(driver,5).until(ec.title_is("My Store"))
     driver.quit()
