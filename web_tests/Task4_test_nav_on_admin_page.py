82bf6e90dd13f783060a5ebfd77779a4a909f767
Ogofman <olenagofman@gmail.com>
2017-06-28 19:08:08 +0300

    fixed wait_until and wait time for submenu

--- web_tests/Task4_test_nav_on_admin_page.py
+++ web_tests/Task4_test_nav_on_admin_page.py
@@ -1,19 +1,33 @@
 from selenium import webdriver
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 driver = webdriver.Firefox()
-driver.implicitly_wait(5)
+
+def waiting(time):
+    driver.implicitly_wait(time)
 
 def test_open_firefox():
     driver.get("http://localhost/litecart/admin/login.php")
     driver.find_element_by_name("username").send_keys("admin")
     driver.find_element_by_name("password").send_keys("admin")
     driver.find_element_by_css_selector("button[name=login]").click()
-    WebDriverWait(driver,3).until(ec.title_is("My Store"))
+    WebDriverWait(driver, 3).until(EC.presence_of_element_located((By.CLASS_NAME, "fa-sign-out")))
 
 def is_submenu_present(locator):
-   return len(driver.find_elements_by_xpath(locator)) > 0
+    if len(driver.find_elements_by_xpath(locator)) > 0:
+        return 1
+    else:
+        waiting(0)
+
+def if_header_present(locator):
+    try:
+        return driver.find_element_by_xpath(locator)
+    except:
+        return 0
 
 def get_sub_menu_after_click(selector, index):
     subMenu = driver.find_elements_by_xpath(selector)
@@ -28,7 +42,7 @@ def test_get_main_menu():
         xpath = ".//*[@id='box-apps-menu']/li[%s]" % i
         if xpath:
             driver.find_element_by_xpath(xpath).click()
-            assert is_submenu_present(".//*[@id='main']/h1")
+            assert if_header_present(".//*[@id='main']/h1")
 
             subMenuSelector = ".//*[@id='box-apps-menu']/li[%s]/ul/li" % i
             if is_submenu_present(subMenuSelector):
@@ -37,6 +51,8 @@ def test_get_main_menu():
                 for index in range(0, lenSubMenu):
                     elem = get_sub_menu_after_click(subMenuSelector, index)
                     elem.click()
-                    assert is_submenu_present(".//*[@id='main']/h1")
+                    assert if_header_present(".//*[@id='main']/h1")
+            else:
+                waiting(1)
 
     driver.quit()
